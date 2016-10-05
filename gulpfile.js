fd40b36a55b464d7820736e4cf50ffe9d55911ab
Paul Sherwood <paul.sherwood@gmail.com>
2016-10-05 16:21:36 +0100

    uses gulp to upload to aws s3 ps

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,28 @@
 var gulp = require('gulp');
+var awspublish = require('gulp-awspublish');
 
 gulp.task('default', function() {
   // place code for your default task here
   console.log("default task")
+});
+
+ 
+var awspublish = require('gulp-awspublish');
+
+// Publish to AWS S3
+gulp.task('publish', function() {
+  var publisher = awspublish.create({
+    region: 'eu-central-1',
+    params: {
+      Bucket: 'ahub-beta-eu-central-1-portal'
+    }
+  });
+  var headers = {
+    'Cache-Control': 'max-age=315360000, no-transform, public'
+  };
+  return gulp.src('dist/**')
+    .pipe(awspublish.gzip())
+    .pipe(publisher.publish(headers))
+    .pipe(publisher.cache())
+    .pipe(awspublish.reporter());
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,13 +23,13 @@ export class AppComponent {
       routerLink: "products/"
     }
   ];
-  products: Object[] = [
-    {name: "dress", imageUrl: "http://www.lindybopusa.com/images/products/zoom/1451996298-20103300.jpg", price: "23.99", sex: "female"},
-    {name: "cowboy boots", imageUrl: "http://www.alphahacks.com/wp-content/uploads/2013/06/Best-Cowboy-Boots.jpg", price: "99.99", sex: "male"},
-    {name: "wellington boots", imageUrl: "http://www.nancyblacks.co.uk/image/cache/data/IMAGES/_0025-441x441.jpg", price: "45.99", sex: "male"},
-    {name: "shorts", imageUrl: "http://www.wigglestatic.com/product-media/5360112002/Altura-Cadence-Baggy-Shorts-Baggy-Cycling-Shorts-Black-SS16-AL30CDBGBS.jpg?w=430&h=430&a=7", price: "19.99", sex: "male"},
-    {name: "shorts", imageUrl: "http://picture-cdn.wheretoget.it/8ay93l-i.jpg", price: "17.49", sex: "female"},
-    {name: "coat", imageUrl: "http://www.dufflecoatsuk.co.uk/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/w/o/womens_check_duffle_coat_red.jpg", price: "55.49", sex: "female"},
-    {name: "coat", imageUrl: "http://www.originalmontgomery.com/media/catalog/product/cache/1/image/17f82f742ffe127f42dca9de82fb58b1/m/e/mens_london_duffle_navy.jpg", price: "45.49", sex: "male"}
-  ];
+  // products: Object[] = [
+  //   {name: "dress", imageUrl: "http://www.lindybopusa.com/images/products/zoom/1451996298-20103300.jpg", price: "23.99", sex: "female"},
+  //   {name: "cowboy boots", imageUrl: "http://www.alphahacks.com/wp-content/uploads/2013/06/Best-Cowboy-Boots.jpg", price: "99.99", sex: "male"},
+  //   {name: "wellington boots", imageUrl: "http://www.nancyblacks.co.uk/image/cache/data/IMAGES/_0025-441x441.jpg", price: "45.99", sex: "male"},
+  //   {name: "shorts", imageUrl: "http://www.wigglestatic.com/product-media/5360112002/Altura-Cadence-Baggy-Shorts-Baggy-Cycling-Shorts-Black-SS16-AL30CDBGBS.jpg?w=430&h=430&a=7", price: "19.99", sex: "male"},
+  //   {name: "shorts", imageUrl: "http://picture-cdn.wheretoget.it/8ay93l-i.jpg", price: "17.49", sex: "female"},
+  //   {name: "coat", imageUrl: "http://www.dufflecoatsuk.co.uk/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/w/o/womens_check_duffle_coat_red.jpg", price: "55.49", sex: "female"},
+  //   {name: "coat", imageUrl: "http://www.originalmontgomery.com/media/catalog/product/cache/1/image/17f82f742ffe127f42dca9de82fb58b1/m/e/mens_london_duffle_navy.jpg", price: "45.49", sex: "male"}
+  // ];
 }
